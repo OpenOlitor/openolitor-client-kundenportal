49503ac251ef8ca198cd8eb61d6bd0143fea0cbf
Alwin Egger <alwin.egger@tegonal.com>
2017-04-18 22:07:24 +0200

    Added Beschrieb; logo can be fetched without login

--- app/scripts/open/lastlieferplanungen.controller.js
+++ app/scripts/open/lastlieferplanungen.controller.js
@@ -23,5 +23,25 @@ angular.module('openolitor-kundenportal')
       $scope.showMore = function() {
         $scope.shownEntries = $scope.shownEntries + 2;
       };
+
+      $scope.hasCommonLieferdatum = function(planung) {
+        if(!angular.isUndefined(planung.commonLieferdatum)) {
+          return true;
+        }
+        var commonDate;
+        var retValue = true;
+        angular.forEach(planung.lieferungen, function(lieferung) {
+          if(angular.isUndefined(commonDate) ||
+            (!angular.isUndefined(lieferung.datum) && lieferung.datum.getTime() === commonDate.getTime())) {
+            commonDate = lieferung.datum;
+          } else {
+            retValue = false;
+          }
+        });
+        if(retValue) {
+          planung.commonLieferdatum = commonDate;
+        }
+        return retValue;
+      };
     }
   ]);
