a9caad37bb27a92d55502c05a3d1932d835411cd
Thomas Rawyler <thomas.rawyler@tegonal.com>
2017-07-10 16:26:30 +0200

    OO-641: upgraded dev setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -437,13 +437,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= openolitor.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -686,7 +679,6 @@ module.exports = function(grunt) {
     'replace:prod',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'rev',
