05a68e7c37bc32c516bae642891b0608b005c283
Alwin Egger <alwin.egger@tegonal.com>
2017-08-24 10:49:48 +0200

    OO-701: Zusatzabos und Zusatzabo-Lieferungen anzeigen

--- app/scripts/abos/list/aboslist.model.js
+++ app/scripts/abos/list/aboslist.model.js
@@ -6,5 +6,10 @@ angular.module('openolitor-kundenportal')
   .factory('AbosListModel', function($resource, API_URL) {
     return $resource(API_URL + 'kundenportal/abos/:id', {
       id: '@id'
-    });
+    }, {
+      zusatzabos: {
+        url: API_URL + 'kundenportal/abos/:aboId/zusatzabos/:id',
+        method: 'GET',
+        isArray: true
+    }});
   });
