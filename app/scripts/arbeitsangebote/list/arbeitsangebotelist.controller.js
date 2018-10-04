9b368ced98b4222972c2060dd2959bc1fc1e9d7f
Alwin Egger <alwin.egger@tegonal.com>
2018-10-04 14:38:53 +0200

    Fixed ordering

--- app/scripts/arbeitsangebote/list/arbeitsangebotelist.controller.js
+++ app/scripts/arbeitsangebote/list/arbeitsangebotelist.controller.js
@@ -2,11 +2,35 @@
 
 /**
  */
-angular.module('openolitor-kundenportal')
-  .controller('ArbeitsangeboteListController', ['$scope', 'NgTableParams', 'ArbeitsangeboteListModel', '$uibModal',
-    '$log', 'alertService', 'gettext', '$http', 'API_URL', 'ooAuthService', 'msgBus',
-    function($scope, NgTableParams, ArbeitsangeboteListModel, $uibModal, $log, alertService, gettext,
-      $http, API_URL, ooAuthService, msgBus) {
+angular
+  .module('openolitor-kundenportal')
+  .controller('ArbeitsangeboteListController', [
+    '$scope',
+    'NgTableParams',
+    'ArbeitsangeboteListModel',
+    '$uibModal',
+    '$log',
+    'alertService',
+    'gettext',
+    '$http',
+    '$filter',
+    'API_URL',
+    'ooAuthService',
+    'msgBus',
+    function(
+      $scope,
+      NgTableParams,
+      ArbeitsangeboteListModel,
+      $uibModal,
+      $log,
+      alertService,
+      gettext,
+      $http,
+      $filter,
+      API_URL,
+      ooAuthService,
+      msgBus
+    ) {
       $scope.arbeitsangebotTableParams = undefined;
 
       $scope.entries = [];
@@ -15,36 +39,43 @@ angular.module('openolitor-kundenportal')
 
       ArbeitsangeboteListModel.query(function(data) {
         $scope.entries = data;
-        if($scope.arbeitsangebotTableParams) {
+        if ($scope.arbeitsangebotTableParams) {
           $scope.arbeitsangebotTableParams.reload();
         }
       });
 
       if (!$scope.arbeitsangebotTableParams) {
-        $scope.arbeitsangebotTableParams = new NgTableParams({ // jshint ignore:line
-          counts: [],
-          sorting: {
-            datum: 'asc'
-          }
-        }, {
-          getData: function(params) {
-            if (!$scope.entries) {
-              return;
+        $scope.arbeitsangebotTableParams = new NgTableParams(
+          {
+            // jshint ignore:line
+            counts: [],
+            sorting: {
+              zeitVon: 'asc'
             }
-            params.total($scope.entries.length);
-            return $scope.entries;
-          }
+          },
+          {
+            getData: function(params) {
+              if (!$scope.entries) {
+                return;
+              }
+              var orderedData = params.sorting
+                ? $filter('orderBy')($scope.entries, params.orderBy())
+                : $scope.entries;
 
-        });
+              params.total(orderedData.length);
+              return orderedData;
+            }
+          }
+        );
       }
 
       $scope.statusClass = function(arbeitsangebot) {
-        if(angular.isDefined(arbeitsangebot)) {
-          switch(arbeitsangebot.status) {
+        if (angular.isDefined(arbeitsangebot)) {
+          switch (arbeitsangebot.status) {
             case 'Erstellt':
             case 'Verschickt':
             case 'MahnungVerschickt':
-              if(arbeitsangebot.faelligkeitsDatum < new Date()) {
+              if (arbeitsangebot.faelligkeitsDatum < new Date()) {
                 return 'fa-circle-o red';
               } else {
                 return 'fa-circle-o';
@@ -62,7 +93,8 @@ angular.module('openolitor-kundenportal')
       $scope.participate = function(arbeitsangebot) {
         var modalInstance = $uibModal.open({
           animation: true,
-          templateUrl: 'scripts/arbeitsangebote/list/arbeitsangebot-participate.html',
+          templateUrl:
+            'scripts/arbeitsangebote/list/arbeitsangebot-participate.html',
           controller: 'ArbeitsangebotParticipateController',
           resolve: {
             arbeitsangebot: function() {
@@ -74,21 +106,32 @@ angular.module('openolitor-kundenportal')
           }
         });
 
-        modalInstance.result.then(function(data) {
-          data.kundeId = ooAuthService.getUser().kundeId;
-          data.personId = ooAuthService.getUser().id;
-          $http.post(API_URL + 'kundenportal/arbeitsangebote',
-            data).then(function() {
-            alertService.addAlert('info', gettext(
-              'Erfolgreich in Arbeitsangebot eingeschrieben'));
-          }, function(error) {
-            alertService.addAlert('error', gettext(
-                'Das Einschreiben war nicht erfolgreich: ') +
-              error.status + ':' + error.statusText);
-          });
-        }, function() {
-          $log.info('Modal dismissed at: ' + new Date());
-        });
+        modalInstance.result.then(
+          function(data) {
+            data.kundeId = ooAuthService.getUser().kundeId;
+            data.personId = ooAuthService.getUser().id;
+            $http.post(API_URL + 'kundenportal/arbeitsangebote', data).then(
+              function() {
+                alertService.addAlert(
+                  'info',
+                  gettext('Erfolgreich in Arbeitsangebot eingeschrieben')
+                );
+              },
+              function(error) {
+                alertService.addAlert(
+                  'error',
+                  gettext('Das Einschreiben war nicht erfolgreich: ') +
+                    error.status +
+                    ':' +
+                    error.statusText
+                );
+              }
+            );
+          },
+          function() {
+            $log.info('Modal dismissed at: ' + new Date());
+          }
+        );
       };
 
       $scope.isParticipating = function(arbeitsangebot) {
@@ -96,10 +139,10 @@ angular.module('openolitor-kundenportal')
       };
 
       $scope.getArbeitseinsatz = function(arbeitsangebot) {
-        if($scope.arbeitseinsatzList) {
+        if ($scope.arbeitseinsatzList) {
           var ret = undefined;
           angular.forEach($scope.arbeitseinsatzList, function(arbeitseinsatz) {
-            if(arbeitseinsatz.arbeitsangebotId === arbeitsangebot.id) {
+            if (arbeitseinsatz.arbeitsangebotId === arbeitsangebot.id) {
               ret = arbeitseinsatz;
             }
           });
@@ -112,7 +155,8 @@ angular.module('openolitor-kundenportal')
         var arbeitseinsatz = $scope.getArbeitseinsatz(arbeitsangebot);
         var modalInstance = $uibModal.open({
           animation: true,
-          templateUrl: 'scripts/arbeitsangebote/list/arbeitsangebot-participate.html',
+          templateUrl:
+            'scripts/arbeitsangebote/list/arbeitsangebot-participate.html',
           controller: 'ArbeitsangebotParticipateController',
           resolve: {
             arbeitsangebot: function() {
@@ -124,21 +168,37 @@ angular.module('openolitor-kundenportal')
           }
         });
 
-        modalInstance.result.then(function(data) {
-          arbeitseinsatz.bemerkungen = data.bemerkungen;
-          arbeitseinsatz.anzahlPersonen = data.anzahlPersonen;
-          $http.post(API_URL + 'kundenportal/arbeitseinsaetze/' + arbeitseinsatz.id,
-            arbeitseinsatz).then(function() {
-            alertService.addAlert('info', gettext(
-              'Arbeitseinsatz erfolgreich verändert.'));
-          }, function(error) {
-            alertService.addAlert('error', gettext(
-                'Arbeitseinsatz ändern nicht erfolgreich: ') +
-              error.status + ':' + error.statusText);
-          });
-        }, function() {
-          $log.info('Modal dismissed at: ' + new Date());
-        });
+        modalInstance.result.then(
+          function(data) {
+            arbeitseinsatz.bemerkungen = data.bemerkungen;
+            arbeitseinsatz.anzahlPersonen = data.anzahlPersonen;
+            $http
+              .post(
+                API_URL + 'kundenportal/arbeitseinsaetze/' + arbeitseinsatz.id,
+                arbeitseinsatz
+              )
+              .then(
+                function() {
+                  alertService.addAlert(
+                    'info',
+                    gettext('Arbeitseinsatz erfolgreich verändert.')
+                  );
+                },
+                function(error) {
+                  alertService.addAlert(
+                    'error',
+                    gettext('Arbeitseinsatz ändern nicht erfolgreich: ') +
+                      error.status +
+                      ':' +
+                      error.statusText
+                  );
+                }
+              );
+          },
+          function() {
+            $log.info('Modal dismissed at: ' + new Date());
+          }
+        );
       };
 
       $scope.sameDay = function(date1, date2) {
--- app/scripts/arbeitseinsaetze/list/arbeitseinsaetzelist.controller.js
+++ app/scripts/arbeitseinsaetze/list/arbeitseinsaetzelist.controller.js
@@ -2,12 +2,41 @@
 
 /**
  */
-angular.module('openolitor-kundenportal')
-  .controller('ArbeitseinsaetzeListController', ['$scope', '$rootScope', 'NgTableParams', 'ArbeitseinsaetzeListModel',
-    'FileUtil', '$location', '$anchorScroll', '$uibModal', 'alertService', 'msgBus', 'gettext', '$http', 'API_URL',
+angular
+  .module('openolitor-kundenportal')
+  .controller('ArbeitseinsaetzeListController', [
+    '$scope',
+    '$rootScope',
+    'NgTableParams',
+    'ArbeitseinsaetzeListModel',
+    'FileUtil',
+    '$location',
+    '$anchorScroll',
+    '$uibModal',
+    'alertService',
+    'msgBus',
+    'gettext',
+    '$http',
+    '$filter',
+    'API_URL',
     'ooAuthService',
-    function($scope, $rootScope, NgTableParams, ArbeitseinsaetzeListModel, FileUtil, $location, $anchorScroll, $uibModal,
-      alertService, msgBus, gettext, $http, API_URL, ooAuthService) {
+    function(
+      $scope,
+      $rootScope,
+      NgTableParams,
+      ArbeitseinsaetzeListModel,
+      FileUtil,
+      $location,
+      $anchorScroll,
+      $uibModal,
+      alertService,
+      msgBus,
+      gettext,
+      $http,
+      $filter,
+      API_URL,
+      ooAuthService
+    ) {
       $scope.arbeitseinsatzTableParams = undefined;
 
       $scope.entries = [];
@@ -16,41 +45,48 @@ angular.module('openolitor-kundenportal')
 
       ArbeitseinsaetzeListModel.query(function(data) {
         $scope.entries = data;
-        if($scope.arbeitseinsatzTableParams) {
+        if ($scope.arbeitseinsatzTableParams) {
           $scope.arbeitseinsatzTableParams.reload();
         }
         var msg = {
-            type: 'ArbeitseinsatzListLoaded',
-            list: $scope.entries
+          type: 'ArbeitseinsatzListLoaded',
+          list: $scope.entries
         };
         msgBus.emitMsg(msg);
       });
 
       if (!$scope.arbeitseinsatzTableParams) {
-        $scope.arbeitseinsatzTableParams = new NgTableParams({ // jshint ignore:line
-          counts: [],
-          sorting: {
-            datum: 'asc'
-          }
-        }, {
-          getData: function(params) {
-            if (!$scope.entries) {
-              return;
+        $scope.arbeitseinsatzTableParams = new NgTableParams(
+          {
+            // jshint ignore:line
+            counts: [],
+            sorting: {
+              zeitVon: 'asc'
             }
-            params.total($scope.entries.length);
-            return $scope.entries;
-          }
+          },
+          {
+            getData: function(params) {
+              if (!$scope.entries) {
+                return;
+              }
+              var orderedData = params.sorting
+                ? $filter('orderBy')($scope.entries, params.orderBy())
+                : $scope.entries;
 
-        });
+              params.total(orderedData.length);
+              return orderedData;
+            }
+          }
+        );
       }
 
       $scope.statusClass = function(arbeitseinsatz) {
-        if(angular.isDefined(arbeitseinsatz)) {
-          switch(arbeitseinsatz.status) {
+        if (angular.isDefined(arbeitseinsatz)) {
+          switch (arbeitseinsatz.status) {
             case 'Erstellt':
             case 'Verschickt':
             case 'MahnungVerschickt':
-              if(arbeitseinsatz.faelligkeitsDatum < new Date()) {
+              if (arbeitseinsatz.faelligkeitsDatum < new Date()) {
                 return 'fa-circle-o red';
               } else {
                 return 'fa-circle-o';
@@ -72,28 +108,42 @@ angular.module('openolitor-kundenportal')
 
       $scope.sameDay = function(date1, date2) {
         return date1.toDateString() === date2.toDateString();
-      }
+      };
 
       $scope.highlightAngebot = function(arbeitsangebotId) {
         $location.hash('arbeitsangebot' + arbeitsangebotId);
         $anchorScroll();
-      }
+      };
 
       $scope.quit = function(arbeitseinsatz) {
-        $http.delete(API_URL + 'kundenportal/arbeitseinsaetze/' + arbeitseinsatz.id).then(function() {
-          alertService.addAlert('info', gettext(
-            'Arbeitsangebot erfolgreich gelöscht.'));
-        }, function(error) {
-          alertService.addAlert('error', gettext(
-              'Arbeitsangebot löschen nicht erfolgreich: ') +
-            error.status + ':' + error.statusText);
-        });
-      }
+        $http
+          .delete(
+            API_URL + 'kundenportal/arbeitseinsaetze/' + arbeitseinsatz.id
+          )
+          .then(
+            function() {
+              alertService.addAlert(
+                'info',
+                gettext('Arbeitsangebot erfolgreich gelöscht.')
+              );
+            },
+            function(error) {
+              alertService.addAlert(
+                'error',
+                gettext('Arbeitsangebot löschen nicht erfolgreich: ') +
+                  error.status +
+                  ':' +
+                  error.statusText
+              );
+            }
+          );
+      };
 
       $scope.edit = function(arbeitseinsatz) {
         var modalInstance = $uibModal.open({
           animation: true,
-          templateUrl: 'scripts/arbeitsangebote/list/arbeitsangebot-participate.html',
+          templateUrl:
+            'scripts/arbeitsangebote/list/arbeitsangebot-participate.html',
           controller: 'ArbeitsangebotParticipateController',
           resolve: {
             arbeitsangebot: function() {
@@ -105,34 +155,58 @@ angular.module('openolitor-kundenportal')
           }
         });
 
-        modalInstance.result.then(function(data) {
-          arbeitseinsatz.bemerkungen = data.bemerkungen;
-          arbeitseinsatz.anzahlPersonen = data.anzahlPersonen;
-          $http.post(API_URL + 'kundenportal/arbeitseinsaetze/' + arbeitseinsatz.id,
-            arbeitseinsatz).then(function() {
-            alertService.addAlert('info', gettext(
-              'Arbeitseinsatz erfolgreich verändert.'));
-          }, function(error) {
-            alertService.addAlert('error', gettext(
-                'Arbeitseinsatz ändern nicht erfolgreich: ') +
-              error.status + ':' + error.statusText);
-          });
-        }, function() {
-          $log.info('Modal dismissed at: ' + new Date());
-        });
+        modalInstance.result.then(
+          function(data) {
+            arbeitseinsatz.bemerkungen = data.bemerkungen;
+            arbeitseinsatz.anzahlPersonen = data.anzahlPersonen;
+            $http
+              .post(
+                API_URL + 'kundenportal/arbeitseinsaetze/' + arbeitseinsatz.id,
+                arbeitseinsatz
+              )
+              .then(
+                function() {
+                  alertService.addAlert(
+                    'info',
+                    gettext('Arbeitseinsatz erfolgreich verändert.')
+                  );
+                },
+                function(error) {
+                  alertService.addAlert(
+                    'error',
+                    gettext('Arbeitseinsatz ändern nicht erfolgreich: ') +
+                      error.status +
+                      ':' +
+                      error.statusText
+                  );
+                }
+              );
+          },
+          function() {
+            $log.info('Modal dismissed at: ' + new Date());
+          }
+        );
       };
 
       msgBus.onMsg('EntityCreated', $scope, function(event, msg) {
-        if ((msg.entity) === 'Arbeitseinsatz' && msg.data.personId === ooAuthService.getUser().id) {
+        if (
+          msg.entity === 'Arbeitseinsatz' &&
+          msg.data.personId === ooAuthService.getUser().id
+        ) {
           $scope.entries.push(msg.data);
           $scope.$apply();
+          $scope.arbeitseinsatzTableParams.reload();
         }
       });
 
       msgBus.onMsg('EntityDeleted', $scope, function(event, msg) {
-        if ((msg.entity) === 'Arbeitseinsatz' && msg.data.personId === ooAuthService.getUser().id) {
-          $scope.entries.splice($scope.entries.indexOf(event.data),1);
+        if (
+          msg.entity === 'Arbeitseinsatz' &&
+          msg.data.personId === ooAuthService.getUser().id
+        ) {
+          $scope.entries.splice($scope.entries.indexOf(event.data), 1);
           $scope.$apply();
+          $scope.arbeitseinsatzTableParams.reload();
         }
       });
     }
