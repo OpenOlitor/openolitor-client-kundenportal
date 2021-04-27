3183f40b89a480276e7b2e29f4492ee738d170e6
Alwin Egger <alwin.egger@tegonal.com>
2021-04-27 21:26:51 +0200

    Show ausgelaufene Abos

--- app/scripts/abos/list/aboslist.controller.js
+++ app/scripts/abos/list/aboslist.controller.js
@@ -3,15 +3,39 @@
 /**
  */
 angular.module('openolitor-kundenportal')
-  .controller('AbosListController', ['$scope', 'AbosListModel',
-    function($scope, AbosListModel) {
+  .controller('AbosListController', ['$scope', 'AbosListModel', 'moment',
+    function($scope, AbosListModel, moment) {
 
       $scope.entries = [];
+      $scope.entriesFiltered = [];
+      $scope.hasAbgelaufen = false;
+      $scope.showAbgelaufen = false;
       $scope.loading = false;
       $scope.model = {};
 
+      $scope.filterEntries = function() {
+        if($scope.showAbgelaufen) {
+          $scope.entriesFiltered = $scope.entries;
+        } else {
+          $scope.entriesFiltered = [];
+          angular.forEach($scope.entries, function(abo) {
+            if(!abo.ende || !moment(abo.ende).isBefore(new Date())) {
+              $scope.entriesFiltered.push(abo);
+            } else {
+              $scope.hasAbgelaufen = true;
+            }
+          });
+        }
+      }
+
       AbosListModel.query(function(data) {
         $scope.entries = data;
+        $scope.filterEntries();
       });
+
+      $scope.showAll = function() {
+        $scope.showAbgelaufen = true;
+        $scope.filterEntries();
+      };
     }
   ]);
