e8dbdbfe6a69400ff1f000ff83c34899e2f35732
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2021-07-07 17:51:18 +0200

    OO#434 limiting the number of participants depending on the number of participants already subscribed

--- app/scripts/arbeitsangebote/list/arbeitsangebot-participate.controller.js
+++ app/scripts/arbeitsangebote/list/arbeitsangebot-participate.controller.js
@@ -4,17 +4,32 @@
  */
 angular.module('openolitor-kundenportal')
   .controller('ArbeitsangebotParticipateController', ['$scope', '$uibModalInstance',
-    '$log', 'arbeitsangebot', 'arbeitseinsatz',
+    '$log', 'arbeitsangebot', 'arbeitseinsatz', 'ArbeitsangeboteListModel','lodash',
 
-    function($scope, $uibModalInstance, $log, arbeitsangebot, arbeitseinsatz) {
+    function($scope, $uibModalInstance, $log, arbeitsangebot, arbeitseinsatz, ArbeitsangeboteListModel,lodash) {
       $scope.arbeitsangebot = arbeitsangebot;
+      $scope.maxVacancies = Number.MAX_ASFE_INTEGER;
+
       if(arbeitsangebot) {
         $scope.formDaten = {
           arbeitsangebotId: arbeitsangebot.id,
           anzahlPersonen: 1,
           bemerkungen: undefined
         };
+        if (!arbeitsangebot.mehrPersonenOk){
+          $scope.maxVacancies = arbeitsangebot.anzahlPersonen - arbeitsangebot.anzahlEingeschriebene;
+        }
       } else if(arbeitseinsatz) {
+        ArbeitsangeboteListModel.query(function(data) {
+          arbeitsangebot = lodash.filter(data, function(aa){
+            return  (aa.id === arbeitseinsatz.arbeitsangebotId);
+          });
+
+          if (arbeitsangebot[0] && !arbeitsangebot[0].mehrPersonenOk){
+            $scope.maxVacancies = arbeitsangebot[0].anzahlPersonen - (arbeitsangebot[0].anzahlEingeschriebene - arbeitseinsatz.anzahlPersonen);
+          }
+
+        });
         $scope.formDaten = {
           arbeitsangebotId: arbeitseinsatz.arbeitsangebotId,
           anzahlPersonen: arbeitseinsatz.anzahlPersonen,
@@ -22,6 +37,18 @@ angular.module('openolitor-kundenportal')
         };
       }
 
+      $scope.getArbeitAngebot = function(arbeitseinsatz) {
+        if ($scope.arbeitseinsatzList) {
+          var ret = undefined;
+          angular.forEach($scope.arbeitseinsatzList, function(arbeitseinsatz) {
+            if (arbeitseinsatz.arbeitsangebotId === arbeitsangebot.id) {
+              ret = arbeitseinsatz;
+            }
+          });
+          return ret;
+        }
+        return undefined;
+      };
 
       $scope.ok = function() {
         $uibModalInstance.close($scope.formDaten);
