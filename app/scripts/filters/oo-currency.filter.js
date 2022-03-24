39cdbc45031328ba6fcad5d3c6600098ad469bc0
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2022-03-24 15:55:50 +0100

    OO#91 adding currency enum to the oocurrency filter

--- app/scripts/filters/oo-currency.filter.js
+++ app/scripts/filters/oo-currency.filter.js
@@ -1,31 +1,24 @@
 'use strict';
 
-angular.module('openolitor-kundenportal').filter('ooCurrency', ['$filter', 'gettextCatalog',function($filter, gettextCatalog) {
+angular.module('openolitor-kundenportal').filter('ooCurrency', ['$filter', 'gettextCatalog','WAEHRUNG','EnumUtil', function($filter, gettextCatalog, WAEHRUNG, EnumUtil) {
   return function(value, currency, showTag) {
+    var waehrungen = EnumUtil.asArray(WAEHRUNG);
     var result = '';
     var symbol = undefined;
     result += $filter('number')(value, 2);
     if(showTag) {
-      let symbol = undefined;
-      if (currency === 'USD') {
-        symbol = '$';
-      } else if (currency === 'CAD'){
-        symbol = '$';
-      } else if (currency === 'EUR'){
-        symbol = '€';
-      } else if (currency === 'GBP' ){
-        symbol = '£';
-      } else {
-        symbol = currency;
+      var enumCurrency = waehrungen.find(i => i.id === currency);
+      if (enumCurrency === undefined){
+        waehrungen.find(i => i.id === "CHF");
       }
-
+      // currency symbol pre number
       if ((gettextCatalog.getCurrentLanguage() === 'en_US') ||
          (gettextCatalog.getCurrentLanguage() === 'de_DE') ||
          (gettextCatalog.getCurrentLanguage() === 'fr_CH') ||
          (gettextCatalog.getCurrentLanguage() === 'de_CH')) {
-        result = symbol + ' ' + result;
+        result = enumCurrency.value + ' ' + result;
       } else {
-        result = result + ' ' + symbol;
+        result = result + ' ' + enumCurrency.value;
       }
     }
     return result;
