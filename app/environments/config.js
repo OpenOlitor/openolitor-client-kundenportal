0184487320a2b9755b39a01fe6d174f6623a9f47
Alwin Egger <alwin.egger@tegonal.com>
2022-11-21 10:55:47 +0100

    updated deps

--- app/environments/config.js
+++ app/environments/config.js
@@ -2,10 +2,10 @@
 
 function getConfig() {
   return {
-    "API_URL": "http://localhost:9000/api-csa1/",
-    "API_WS_URL": "http://localhost:9000/api-csa1/ws",
+    "API_URL": "http://localhost:9000/api-try/",
+    "API_WS_URL": "http://localhost:9000/api-try/ws",
     "ENV": "dev",
-    "version": "2.5.7",
+    "version": "2.6.0",
     "sendStats": true
   };
 }
